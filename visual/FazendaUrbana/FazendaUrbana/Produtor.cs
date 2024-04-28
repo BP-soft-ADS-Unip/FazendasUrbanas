b535dc91d9ae44f181668772a3ea358434f27fef
gpapa-sam <frbrigadas@gmail.com>
2024-04-28 11:56:04 -0300

    organização e inclusão do código c# controle fornecedores

---
+++ visual/FazendaUrbana/FazendaUrbana/Produtor.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FazendaUrbana
+{
+	internal class Produtor
+	{
+		public string Email { get; set; }
+		public string Nome { get; set; }
+		public string Celular { get; set; }
+		public string Senha { get; set; }
+
+		public void status()
+			{
+				Console.WriteLine("Nome: {0}", Nome);
+				Console.WriteLine("Email: {0}", Email);
+				Console.WriteLine("Celular: {0}", Celular);
+			}
+
+		public int login()
+		{
+			int i = 0;
+			while (i < 3)
+			{
+                Console.WriteLine("Entre com Email: ");
+				string email = Console.ReadLine();
+				if (Email.Equals(email))
+				{
+                    Console.WriteLine("Entre com a senha");
+					break;
+                }
+				else
+				{
+					Console.WriteLine("Erro!!!!\nEmail inexistente!!!");
+					i++;
+				}
+			}
+			if (i == 3)
+				return 0;
+			i = 0;
+			while (i < 3)
+			{
+				string senha = Console.ReadLine();
+				if (Senha.Equals(senha))
+				{
+					Console.WriteLine("Você está Logado");
+					this.status();
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Senha Incorreta");
+					i++;
+				}
+			}
+			if (1 == 3)
+				Console.WriteLine("Login Cancelado!!!");
+			return 1;
+				
+			
+				
+            
+			
+		}
+
+
+	}
+
+	
+}
+
+
---
+++ visual/FazendaUrbana/FazendaUrbana/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+using Npgsql;
+/*
+string connectionString = "Server=localhost;Port=5432;Database=fazendau;User ID=postgres;Password=9503;";
+using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+{
+    connection.Open();
+
+    NpgsqlCommand command = new NpgsqlCommand("INSERT INTO PRODUTOR(EMAIL, CELULAR, NOME, SENHA)" +
+        "VALUES (@EMAIL, @CELULAR, @NOME,@SENHA)", connection);
+
+    command.Parameters.AddWithValue("@EMAIL", "BIA@UOL");
+    command.Parameters.AddWithValue("@celular", "456897");
+    command.Parameters.AddWithValue("@NOME", "beatriz");
+    command.Parameters.AddWithValue("@SENHA", "12345");
+
+    command.ExecuteNonQuery();
+
+    connection.Close();
+
+}
+*/
+namespace FazendaUrbana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Produtor teste2 = new Produtor();
+             teste2.Nome = "JOSE";
+             teste2.Email = "jose@ui";
+             teste2.Celular = "4569822";
+             teste2.Senha = "1234";
+            /*
+                         teste.login();
+                         //teste.status();*/
+            //string comando_sql = "INSERT INTO PRODUTOR(EMAIL, CELULAR, NOME, SENHA) VALUES (@EMAIL, @CELULAR, @NOME, @SENHA)";
+            string comando_sql = "SELECT * FROM produtor";
+            BD teste = new BD(2, comando_sql);
+            teste.query(teste2.Email, teste2.Celular, teste2.Nome, teste2.Senha);
+
+            
+
+
+
+
+        }
+    }
+}
+
